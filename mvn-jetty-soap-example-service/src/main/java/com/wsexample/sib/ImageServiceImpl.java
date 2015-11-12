2f4fd0b553a125875cd0b21e8a533a33a8701f80
gmm <gustavo.maranga@gmail.com>
2015-11-12 17:01:30 -0300

    Google Image lookup added when the ImageService cant find the file

--- mvn-jetty-soap-example-service/src/main/java/com/wsexample/sib/ImageServiceImpl.java
+++ mvn-jetty-soap-example-service/src/main/java/com/wsexample/sib/ImageServiceImpl.java
@@ -1,13 +1,23 @@
 package com.wsexample.sib;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.imageio.ImageIO;
 import javax.jws.WebService;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.xml.ws.WebServiceException;
 import javax.xml.ws.soap.MTOM;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.wsexample.sei.ImageService;
 
@@ -27,7 +37,11 @@ public class ImageServiceImpl implements ImageService {
 		try {
 			img = ImageIO.read(file);
 		} catch (IOException e) {
-			throw new WebServiceException("Failed reading file", e);
+			try{
+				return googleLookup(fileName);
+			}catch(Exception ex){
+				throw new WebServiceException(ex);
+			}
 		}
 		return img;
 	}
@@ -41,6 +55,32 @@ public class ImageServiceImpl implements ImageService {
 		}
 		
 		throw new WebServiceException("Upload Failed!");
+	}
+	
+	private Image googleLookup(String query) throws Exception{
+ 
+			URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+query);
+            URLConnection connection = url.openConnection();
+            Image image = null;
+            String line;
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            JSONObject json = new JSONObject(builder.toString());
+            
+            JSONArray array = json.getJSONObject("responseData").getJSONArray("results");
+            
+            for(int i = 0; i < array.length(); i++){
+            	String imageUrl = json.getJSONObject("responseData").getJSONArray("results").getJSONObject(i).getString("url");
+            	try{
+            		image = ImageIO.read(new URL(imageUrl));
+            	}catch(IOException e){}
+            }
+            
+            return image;
 	}
 
 }
